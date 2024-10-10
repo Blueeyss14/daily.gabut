30be8b872a771ccb350641120d97bdec9c94c02b
unknown <delkanoberutu8@gmail.com>
2024-10-11 00:09:28 +0700

    nanti diupdate

--- kedua/src/App.jsx
+++ kedua/src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import { useState } from 'react';
+import { useState } from 'react'
 import { useEffect } from 'react';
 
 const App = () => {
-
   const current_theme = localStorage.getItem('current_theme');
   const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
 
   useEffect(() => {
-    localStorage.setItem('current_theme', theme);
-  }, [theme])
-
+    localStorage.getItem('current_theme', theme)
+  }, [theme]);
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme}/>
--- kedua/src/components/navbar/Navbar.jsx
+++ kedua/src/components/navbar/Navbar.jsx
@@ -1,35 +1,37 @@
 import React from 'react'
+import './navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
-import './navbar.css'
-
 const Navbar = ({theme, setTheme}) => {
-    const toggle_mode = () => {
 
-        if (theme == 'light') {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
-    }
+const toggle_mode = () => {
+  if (theme == 'light') {
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+}
 
   return (
-    <div className='navbar'>
-      <h1>Logo</h1>
-      <ul>
-        <li>Home</li>
-        <li>Product</li>
-        <li>Contact</li>
-        <li>About</li>
-      </ul>
-      
-      <div className='toggle-icon'>
-        <FontAwesomeIcon icon={theme == 'light'? faMoon : faSun} onClick={() => {toggle_mode()}}/>
-      </div>
+    <div>
+      <div className='navbar'>
+        <h1>LOGO</h1>
 
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Support</li>
+        </ul>
+
+        <div className='toggle-icon'>
+          <FontAwesomeIcon icon = {theme == 'light' ? faMoon : faSun} onClick={() => toggle_mode()}/>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Navbar
+
+
