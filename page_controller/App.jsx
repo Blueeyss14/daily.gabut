c356b2ab9250b9694e1ac0b2ffcf87f52ef1d6ab
unknown <delkanoberutu8@gmail.com>
2025-01-29 15:47:17 +0700

    update this

--- page_controller/App.jsx
+++ page_controller/App.jsx
@@ -1,30 +1,13 @@
-import React from "react";
+import React from 'react'
+import MainHome from './views/home/MainHome'
 
-import PageController from "./controller/PageController";
-
-const item = [
-  <h1 key="home">Home</h1>, 
-  <h1 key="project">Project</h1>
-];
-const conColor = [
-  "bg-red-300", 
-  "bg-green-300"
-];
 
 const App = () => {
   return (
-    <div className="w-screen h-screen bg-blue-200 flex justify-center items-center snap-x snap-mandatory">
-      <PageController/>
-      <div className="w-[160px] h-[50px] bg-green-400 fixed bottom-0 mb-10 rounded-2xl flex overflow-hidden">
-        {item.map((items, index) => (
-          <div
-            className={`${conColor[index]} flex-1 flex justify-center items-center cursor-pointer`}>
-            <div key={index}>{items}</div>
-          </div>
-        ))}
-      </div>
+    <div>
+      <MainHome></MainHome>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ page_controller/views/home/MainHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+
+const MainHome = () => {
+  return (
+    <div>
+        <Carousel></Carousel>
+    </div>
+  )
+}
+
+export default MainHome
