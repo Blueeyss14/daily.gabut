4ee7355528f7d08ef2c16e0b9661bebbf2f78f23
unknown <delkanoberutu8@gmail.com>
2025-06-22 19:09:56 +0700

    ui kit

---
+++ game ui kit/App.jsx
@@ -0,0 +1,12 @@
+import Button1 from "./components/Button1"
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Button1/>
+    </div>
+  )
+}
+
+export default App
---
+++ game ui kit/components/Button1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Button1 = () => {
+  return (
+
+    <div className='bg-amber-700 rounded-[13px] bg-gradient-to-b from-amber-500 to-amber-700 shadow-md p-[1px]'>
+        
+    <div className='w-30 h-10 rounded-[12px] overflow-clip bg-gradient-to-b from-amber-500 to-amber-600'>
+        <div className='flex-1 h-5 bg-gradient-to-b  from-amber-100 to-transparent'></div>
+        <h1 className='absolute'>Button</h1>
+    </div>
+    </div>
+
+
+
+  )
+}
+
+export default Button1
+
+// border-amber-700 border-3
